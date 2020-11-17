25bc01865d26265bef8e17872a910e6051f63325
Jocel <jocel.silva@numeria.com.br>
2020-11-17 20:23:37 -0300

    Codigo com classe object e alguns overrides

---
+++ Object/Leilao.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Object
+{
+    class Leilao
+    {
+        private string Nome { get; set; }
+        private string Artigo { get; set; }
+        private double Lance { get; set; }
+
+        public Leilao(string nome, string artigo, double lance)
+        {
+            Nome = nome;
+            Artigo = artigo;
+            Lance = lance;
+        }
+
+        //sobescrita do metodo equals
+        public override bool Equals(object obj)
+        {
+            Leilao outroLeilao = (Leilao)obj; // recebendo objeto
+            //Leilao outroLeilao2 = obj as Leilao; // recebendo objeto de outros tipos
+
+            return
+                Artigo == outroLeilao.Artigo &&
+                Lance == outroLeilao.Lance
+
+        }
+        public bool ComparaLance(string nome, string artigo, double lance)
+        {
+            Leilao outroLeilao = new Leilao(nome, artigo, lance);
+
+            if (this.Equals(outroLeilao))
+            {
+
+            }
+        }
+
+        //sobreescrita do metodo tostring()
+        public override string ToString()
+        {
+            return "Novo metodo ToString";
+        }
+    }
+}
---
+++ Object/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomePessoa = "Livia";
+            string objetoLeilao = "Barbie";
+            double valor = 52.56;
+
+            var l = new Leilao(nomePessoa, objetoLeilao, valor);
+
+            string classeTostring = l.ToString();
+
+            //Console.WriteLine(l);
+            //Console.WriteLine(classeTostring);
+
+            //sobrescrita do metodo tostring()
+            Console.WriteLine(l.ToString());
+        }
+    }
+}
